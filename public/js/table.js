64bc5fce0453b6b4f48b354833e7aec8b2f7a2ed
yiwang <yiwang3@andrew.cmu.edu>
2020-03-22 23:51:41 -0700

    fix

--- public/js/table.js
+++ public/js/table.js
@@ -3,9 +3,9 @@ function tableCreate(row_num=30, col_num=30) {
     const tbl = document.createElement("table");
     const tblBody = document.createElement("tbody");
     let count = 0;
-    for (let j = 0; j <= row_num; j++) {
+    for (let j = 0; j < row_num; j++) {
         const row = document.createElement("tr");
-        for (let i = 0; i <= col_num; i++) {
+        for (let i = 0; i < col_num; i++) {
             const cell = document.createElement("td");
             cell.id = `t-${count}`;
             count++;
